21381a83d0ee44d58c8b729ddfc6d5a940b37ad8
rchavelas90 <rchavelas90@gmail.com>
2013-10-14 09:22:00 -0500

    Clarify comments in east_nroths

--- east_norths_logist.R
+++ east_norths_logist.R
@@ -1,14 +1,13 @@
 # Haversine formula is appropriate for calculating distances from lat/long
 
 EarthRad=6371000 ##R# [in meters] 
-
 haverDist<-function(aLong,aLat,bLong,bLat){
  dLat=2*pi*(bLat-aLat)/360.0; dLon=2*pi*(bLong-aLong)/360.0
  a=(sin(dLat/2))^2+cos(2*pi*aLat/360)*cos(2*pi*bLat/360)*(sin(dLon/2)^2)
  return(EarthRad*2*atan2(sqrt(a),sqrt(1-a)))
 }
 
-## Load CSV file with coordinates of 
+## Load CSV file with coordinates (Column names: Description, Latitude and Longitude)
 df <- read.csv("locations.csv",check.names=FALSE,header=T,skip=0)
 df
 colnames(df)=c("Description","Latitude","Longitude")
@@ -19,17 +18,16 @@ df$East=haverDist(df[1,"Longitude"],df[1,"Latitude"],df$Longitude,df[1,"Latitude
 df$North=haverDist(df[1,"Longitude"],df[1,"Latitude"],df[1,"Longitude"],df$Latitude)*
  sign(df$Latitude-df[1,"Latitude"])
 df$distFromWh <- sqrt(df$East^2+df$North^2)
-df
+
 # write a new CSV file with the data on it
-#write.csv(df,file="locations2.csv")
+write.csv(df,file="locations2.csv")
 
 
 # Map of locations
-df
 df2 <- data.frame(loc=numeric(nrow(df)),factor=numeric(nrow(df)))
 df2$loc=paste(df$Latitude[1:nrow(df)],df$Longitude[1:nrow(df)],sep=":")
 df2$factor <- df$Description
-df2
+
 
 library(googleVis)
 g2 <- gvisMap(df2,"loc",
@@ -39,4 +37,6 @@ g2 <- gvisMap(df2,"loc",
                            enableScrollWheel=T))
 plot(g2)
 
-## Source http://www.movable-type.co.uk/scripts/latlong.html
+## Sources
+## http://lwlss.net/GarminReports/GarminFunctions.R
+## http://www.movable-type.co.uk/scripts/latlong.html
